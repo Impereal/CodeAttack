76e402425c85ad8cda10e9f64c0f58dbbe15ec96
Leean <lxz7432@g.coppellisd.com>
2021-01-10 16:17:54 -0600

    Update loadingScreen.py
    Loading Screen, but I'm not sure about the image PATH, and how it works with git

--- Code/LoadingScreen/loadingScreen.py
+++ Code/LoadingScreen/loadingScreen.py
@@ -10,11 +10,38 @@ Version: 1.0.0
 """
 
 # Imports
-import arcade
-import pygame
-import tkinter
-import PIL
+from tkinter import *
+from PIL import ImageTk,Image
+
 
 # Function to display the initial loading screen of the application
 def displayLoadingScreen():
-    pass
+    #Create Window
+    root = Tk()
+
+    #Window dimensions
+    # getting screen's height in pixels 
+    height = root.winfo_screenheight()  
+    # getting screen's width in pixels 
+    width = root.winfo_screenwidth()
+    root.state("zoomed")
+
+    #Naming window
+    root.title("Loading Screen")
+    canvas=Canvas(root, width=width, height=height)
+    
+    #Set background color
+    canvas.configure(bg="black")
+    canvas.pack()
+
+    #displaying the image
+    global img
+    img = ImageTk.PhotoImage(Image.open(r"C:\Users\Kids.DESKTOP-2OS06UD\Desktop\Python for TSA\GitHub\CodeAttack\CodeAttack\Resources\Images\TSALogoTransparent.png"))
+    canvas.create_image(width/2,height/2, image=img)
+    mainloop()
+
+#Run function
+displayLoadingScreen()
+
+
+
